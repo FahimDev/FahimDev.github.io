c5d974239f28e0382c74b2771713205b3489f790
fahimdev <fahim.arif0373@outlook.com>
2024-09-27 17:31:08 +0600

    changes out dir from dist to docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/fahimdev.github.io/",
+  base: "/fahimdev.github.io/docs",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build : {
+    outDir: "docs"
+  }
 })
