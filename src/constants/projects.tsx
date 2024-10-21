08bca2c135d975c96c4d3b43eac7f5c6198a405c
fahimdev <fahim.arif0373@outlook.com>
2024-10-22 00:07:29 +0600

    Updates Hotelereum Project Info and Logos

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -3,32 +3,47 @@ import { FaGithub, FaTwitter } from "react-icons/fa";
 export const PROJECTS: any[] = [
     {
         title: "Hotelereum",
-        subtitle: "NFT Marketplace for Hotels",
+        subtitle: "NFT Marketplace for Hotel Reservation & Reselling",
         slug: "hotelereum",
-        client: "Nigel Salem",
-        location: "London, United Kingdom",
-        description: `Hotelereum is an innovative hotel booking platform that seamlessly integrates NFT (Non-Fungible Token) 
-        technology into the travel and hospitality industry. It allows users to book accommodations and transform their 
-        reservations into unique digital assets, represented as NFTs. These NFTs grant ownership over bookings, allowing travelers 
-        not only to secure their stay but also to trade, sell, or transfer their reservations on various marketplaces.`,
-        techs: ["JavaScript", "Python", "Solidity"],
+        client: "InterCo*******tal",
+        location: "UK",
+        description: `It is a dApp of Marketplace where people can book hotels and resell them in proprietary or 3rd party marketplaces. 
+        Different hotels can join here as a partner to enable their services in Web3 networks. Reservations are identified as NFT digital objects. 
+        So the reservation owner can actually own their bookings and can resell those. So refund claiming will not  an issue anymore.
+        🕵 Smart Contract Audit: 📍 Rekt Test, 📍 Tincho Method, 📍 Cyfrin Standard`,
+        techs: ["Solidity", "Polygon_PoS", "NestJs", "NextJs", "Rainbow_Kit", "PostgreSQL", "Auth0", "Chinlink_Oracle", "The_Graph",
+             "Waffle_Chai", "Solidity Metrics (IDE Extension)", "Slither", "Aderyn"],
         tech_icons: {
-            JavaScript: "https://picsum.photos/200",
-            Python: "https://picsum.photos/200",
-            Solidity: "https://picsum.photos/200",
+            NextJs: "/images/tech_logo/nestJS.jpg",
+            NestJs: "/images/tech_logo/nextJS.webp",
+            Rainbow_Kit: "/images/tech_logo/Rainbow.png",
+            Auth0: "/images/tech_logo/auth0.svg",
+            Solidity: "/images/tech_logo/solidity.jpg",
+            Hardhat: "/images/tech_logo/hardhat.jpg",
+            Chainlink: "/images/tech_logo/chainlink.webp",
+            Polygon: "/images/tech_logo/polygon.png",
+            Slither: "/images/tech_logo/slither.jpg",
+            Aderyn: "/images/tech_logo/aderyn.png"
         },
-        cover_img: "https://picsum.photos/200",
+        cover_img: "/images/projects/1/1.png",
         github_url: "https://github.com/antonin686/hotelereum",
         view_url: "https://github.com/antonin686/hotelereum",
         business_challenges: [
-            "Designing user interactions in Web3 for public services on blockchain posed significant challenges for the public",
-            "Designing user interactions in Web3 for public services on blockchain posed significant challenges for the public",
-            "Designing user interactions in Web3 for public services on blockchain posed significant challenges for the public",
+            "Register hotels as Partners in the primary market.",
+            "Inventory Management of hotel rooms.",
+            "Primary Marketplace for hotel booking.",
+            "Minting NFTs as Digital Twin of hotel Reservation.",
+            "Reselling reservation into secondary marketplaces.",
+            "Purchase price with Oracle live exchange rate from utility tokens to Stable Coins.",
+            "Proprietary marketplace for reselling reservation NFT.",
+            "Royalty/ commission collection from 3rd party marketplaces.",
+            "Split booking from single NFT to multiple NFTs.",
         ],
         solutions: [
-            "Designing user interactions in Web3 for public services on blockchain posed significant challenges for the public",
-            "Designing user interactions in Web3 for public services on blockchain posed significant challenges for the public",
-            "Designing user interactions in Web3 for public services on blockchain posed significant challenges for the public",
+            "To ensure compliance with GDPR law in our system, we have integrated Auth0 into our architecture.",
+            "To obtain on-chain real-time exchange and swap rates of different currencies, Chainlink Oracle has been integrated.",
+            "The From and To dates were designed as dynamic metadata, enabling ERC721 NFTs to function as splittable reservations.",
+            "To support the blueprint NFT marketplace microstructure, research was published on royalty collection for hotel partners from third-party marketplaces and stablecoin payment integration. 📜 DOI: 10.1109/icbc59979.2024.10634338",
         ],
         find_it_on: {
             github: {
@@ -42,15 +57,19 @@ export const PROJECTS: any[] = [
         },
         gallery: [
             {
-                url: "https://picsum.photos/200",
+                url: "/images/projects/1/2.png",
                 alt: "image",
             },
             {
-                url: "https://picsum.photos/200",
+                url: "/images/projects/1/3.png",
                 alt: "image",
             },
             {
-                url: "https://picsum.photos/200",
+                url: "/images/projects/1/4.png",
+                alt: "image",
+            },
+            {
+                url: "/images/projects/1/5.png",
                 alt: "image",
             },
         ],
