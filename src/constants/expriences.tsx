d01ae11ef2e541929e3d835f83f81745c5960b3d
fahimdev <fahim.arif0373@outlook.com>
2024-11-02 13:18:03 +0600

    Updates Job History

--- src/constants/expriences.tsx
+++ src/constants/expriences.tsx
@@ -1,20 +1,39 @@
 export const EXPERIENCES = [
     {
-        company: "BrainStation 23",
+        company: "BrainStation 23 PLC.",
         position: "Software Engineer",
-        duration: "July 2022 - Present",
+        duration: "March 2022 - Present",
         location: "Dhaka, Bangladesh",
-        points: ["Web Development", "Machine Learning", "Data Science"],
+        points: ["Web3 Engineering", "System Architect", "Cryptography R&D"],
         logo: "/images/bs23.jpeg",
-        link: "#"
+        link: "https://brainstation-23.com/?1"
     },
     {
-        company: "RRAD",
-        position: "Software Engineer",
-        duration: "July 2022 - Present",
-        location: "Dhaka, Bangladesh",
-        points: ["Web Development", "Machine Learning", "Data Science"],
-        logo: "/images/bs23.jpeg",
-        link: "#"
+        company: "Cloud Height Technology LTD. (CHTL)",
+        position: "Junior Software Engineer",
+        duration: "January 2022 - April 2022",
+        location: "Chittagong, Bangladesh",
+        points: ["Backend Engineer"],
+        logo: "/images/chtl.png",
+        link: "https://cloud-height.com/"
     },
+    {
+        company: "Robust Research and Development LTD. (RRAD)",
+        position: "Associate Software Engineer",
+        duration: "December 2020 - December 2021",
+        location: "Dhaka, Bangladesh",
+        points: ["Backend Engineer", "Flutter Mobile App Development"],
+        logo: "/images/rrad.jpg",
+        link: "https://rrad.ltd/"
+    },
+    {
+        company: "Export Import Bank of Bangladesh PLC. (EXIM)",
+        position: "IT Intern",
+        duration: "October 2020 - December 2020",
+        location: "Dhaka, Bangladesh",
+        points: ["Data-center operations and maintenance", "Banking Fundamentals"],
+        logo: "/images/exim.jpg",
+        link: "https://www.eximbankbd.com/"
+    },
+
 ];
