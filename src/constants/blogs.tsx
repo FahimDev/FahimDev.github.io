d64c5dc07a4a4546882ff3ddf4f10eb5ee71b6ad
fahimdev <fahim.arif0373@outlook.com>
2024-11-07 23:41:11 +0600

    Updates Blog Section

--- src/constants/blogs.tsx
+++ src/constants/blogs.tsx
@@ -1,23 +1,39 @@
 export const BLOGS: any[] = [
     {
-        title: "Hotelereum",
-        slug: "hotelereum",
-        description: "a hotel booking platform incorporating NFT technology",
-        image: "https://picsum.photos/200",
+        title: "Paradigm Shift from Electronic Money to Stablecoins",
+        slug: "stablecoin-the-ultimate-future-of-electronic-money",
+        description: `𝘍𝘳𝘰𝘮 𝘳𝘦𝘤𝘦𝘯𝘵 𝘤𝘳𝘺𝘱𝘵𝘰 𝘯𝘦𝘸𝘴, 𝘪𝘵 𝘪𝘴 𝘤𝘭𝘦𝘢𝘳 𝘵𝘩𝘢𝘵 𝘴𝘵𝘢𝘣𝘭𝘦𝘤𝘰𝘪𝘯𝘴 𝘩𝘢𝘷𝘦 𝘴𝘪𝘨𝘯𝘪𝘧𝘪𝘤𝘢𝘯𝘵 𝘱𝘰𝘵𝘦𝘯𝘵𝘪𝘢𝘭 𝘧𝘰𝘳 𝘢𝘵𝘰𝘮𝘪𝘤 𝘵𝘳𝘢𝘯𝘴𝘢𝘤𝘵𝘪𝘰𝘯𝘴 𝘢𝘯𝘥 𝘥𝘪𝘨𝘪𝘵𝘢𝘭 𝘱𝘢𝘺𝘮𝘦𝘯𝘵𝘴 𝘪𝘯 𝘵𝘩𝘦 𝘯𝘦𝘢𝘳 𝘧𝘶𝘵𝘶𝘳𝘦.`,
+        image: "images/blog/4.png",
+        link: "https://www.linkedin.com/pulse/paradigm-shift-from-electronic-money-stablecoins-md-ariful-islam-gignc/",
+    },
+    {
+        title: "Strategies to Design Solidity Custom Event Topics 📡💬",
+        slug: "strategies-to-design-solidity-event-topics-for-listening-transactions",
+        description: `💡 Guiding the Web3 Engineer to Tune into the Pulse of the Blockchain with Solidity Events and Node Provider's WebSocket Endpoint ⚡️
+        dApps are built using Event-Driven Architecture, where Blockchain events trigger Dapp actions. Solidity Events are crucial, enabling cheap logging and real-time reactions to state changes.`,
+        image: "images/blog/3.jpeg",
         link: "https://picsum.photos/200",
     },
     {
-        title: "Hotelereum2",
-        slug: "hotelereum",
-        description: "a hotel booking platform incorporating NFT technology",
-        image: "https://picsum.photos/200",
-        link: "https://picsum.photos/200",
+        title: "How We Fixed Missing Wei Tokens in MATIC 🪙",
+        slug: "decimal-scaling-mechanism-for-matic-priced-nft-fractional-issue",
+        description: `The main problem is the mismatch in decimal places between cryptocurrencies 
+        (e.g., MATIC, USDC) and fiat currencies. Cryptocurrencies often have extremely small sub-units 
+        (like wei or gwei), while fiat only has 2 decimal places. This can lead to loss of fractional value 
+        when converting between these assets in smart contracts, which is a common issue developers face when 
+        building DeFi applications.`,
+        image: "images/blog/2.jpeg",
+        link: "https://www.linkedin.com/pulse/decimal-scaling-scientific-notation-how-we-fixed-missing-islam-69bjc",
     },
     {
-        title: "Hotelereum3",
-        slug: "hotelereum",
-        description: "a hotel booking platform incorporating NFT technology",
-        image: "https://picsum.photos/200",
-        link: "https://picsum.photos/200",
+        title: "Enabling Web 2.0 users to perform Web3 transactions on Fabric Network 🪪",
+        slug: "hyperledger-fabric-wallet-management",
+        description: `A tested implementation approach and published Q1 journal article. 
+        The Fabric Wallet contains critical components like a digital certificate, private key, 
+        and membership ID that authenticate and authorize users in the Hyperledger Fabric network. 
+        Without a Fabric Wallet, users cannot access or perform transactions on the private blockchain network. 
+        DOI: https://doi.org/10.1109/ACCESS.2023.3279724`,
+        image: "images/blog/1.png",
+        link: "https://www.linkedin.com/pulse/enabling-web-20-users-perform-web3-transactions-without-islam/",
     }
 ];
