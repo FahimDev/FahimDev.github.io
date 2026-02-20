29ab7a58ff59d53720566e78296bac2b70c4239f
FahimDev <ariful.islam@brainstation-23.com>
2026-02-20 14:38:54 +0600

    Added new Data of AWS Hackathon Winning

--- src/constants/most-proud-ofs.tsx
+++ src/constants/most-proud-ofs.tsx
@@ -1,5 +1,11 @@
 export const MOST_PRODUCT_OFS = [
     {
+        title: "AWS Global Vibe: AI Coding Hackathon",
+        subtitle: "Slalom.com Half day Agentic AI workshops 2025",
+        award: "Winner of AI and Web3 Integration Category",
+        link: "https://dorahacks.io/hackathon/awsvibecoding/winner",
+    },
+    {
         title: "International Blockchain Olympiad",
         subtitle: "(IBCOL 2021)",
         award: "Merit Award",
